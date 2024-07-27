468915ad2b9c9ed22fdab6658322ed32cc0742d3
Devansh Raut <96239425+devanshraut@users.noreply.github.com>
2024-07-27 20:46:53 +0530

    Update resetPassword.js

--- server/controllers/resetPassword.js
+++ server/controllers/resetPassword.js
@@ -25,11 +25,11 @@ exports.resetPasswordToken = async (req, res) => {
     console.log("DETAILS", updatedDetails)
 
     // const url = `http://localhost:3000/update-password/${token}`
-    const url = `https://studynotion-edtech-project.vercel.app/update-password/${token}`
+    const url = `https://edu-flex-devansh-rauts-projects.vercel.app/update-password/${token}`
 
     await mailSender(
       email,
-      "Password Reset",
+      "Password Reset Link",
       `Your Link for email verification is ${url}. Please click this url to reset your password.`
     )
 
