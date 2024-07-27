b24bda0f765c79509c8bb2cfa16997734b24f34e
Devansh Raut <96239425+devanshraut@users.noreply.github.com>
2024-07-27 20:33:38 +0530

    Update passwordUpdate.js

--- server/mail/templates/passwordUpdate.js
+++ server/mail/templates/passwordUpdate.js
@@ -56,7 +56,7 @@ exports.passwordUpdated = (email, name) => {
     <body>
         <div class="container">
             <a href="https://edu-flex-devansh-rauts-projects.vercel.app/"><img class="logo"
-                    src="https://app.gemoo.com/share/image-annotation/675826409650470912?codeId=vzawEgXKAAo3d&origin=imageurlgenerator" alt="EDUFLEX Logo"></a>
+                    src="https://i.postimg.cc/XvsbZSRf/pixelcut-export.png" alt="EDUFLEX Logo"></a>
             <div class="message">Password Update Confirmation</div>
             <div class="body">
                 <p>Hey ${name},</p>
