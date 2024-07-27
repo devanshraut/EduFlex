f3e224411142fab13b7835a874e9869ca6213b2d
Devansh Raut <96239425+devanshraut@users.noreply.github.com>
2024-07-27 20:32:55 +0530

    Update courseEnrollmentEmail.js

--- server/mail/templates/courseEnrollmentEmail.js
+++ server/mail/templates/courseEnrollmentEmail.js
@@ -67,7 +67,7 @@ exports.courseEnrollmentEmail = (courseName, name) => {
   
   <body>
       <div class="container">
-          <a href="https://edu-flex-devansh-rauts-projects.vercel.app/"><img class="logo" src="https://app.gemoo.com/share/image-annotation/675826409650470912?codeId=vzawEgXKAAo3d&origin=imageurlgenerator"
+          <a href="https://edu-flex-devansh-rauts-projects.vercel.app/"><img class="logo" src="https://i.postimg.cc/XvsbZSRf/pixelcut-export.png"
                   alt="EDUFLEX Logo"></a>
           <div class="message">Course Registration Confirmation</div>
           <div class="body">
