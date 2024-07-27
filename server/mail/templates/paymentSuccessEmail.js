552159abfc88f5ecf3bce843236dee51b74d2b04
Devansh Raut <96239425+devanshraut@users.noreply.github.com>
2024-07-27 20:34:04 +0530

    Update paymentSuccessEmail.js

--- server/mail/templates/paymentSuccessEmail.js
+++ server/mail/templates/paymentSuccessEmail.js
@@ -67,7 +67,7 @@ exports.paymentSuccessEmail = (name, amount, orderId, paymentId) => {
     
     <body>
         <div class="container">
-            <a href="https://edu-flex-devansh-rauts-projects.vercel.app/"><img class="logo" src="https://app.gemoo.com/share/image-annotation/675826409650470912?codeId=vzawEgXKAAo3d&origin=imageurlgenerator"
+            <a href="https://edu-flex-devansh-rauts-projects.vercel.app/"><img class="logo" src="https://i.postimg.cc/XvsbZSRf/pixelcut-export.png"
                     alt="EDUFLEX Logo"></a>
             <div class="message">Course Payment Confirmation</div>
             <div class="body">
